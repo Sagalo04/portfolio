962ef5cee7ae735950bed33d7e3071ec221d1f82
Sagalo04 <santiago.garcna@uao.edu.co>
2022-01-06 21:16:51 -0500

    _blank links

--- src/components/Seccion/Welcome/SocialLinks/SocialLinks.jsx
+++ src/components/Seccion/Welcome/SocialLinks/SocialLinks.jsx
@@ -1,32 +1,57 @@
-import React from 'react';
-import Styles from "./SocialLinks.module.scss"
-import { Icon } from '@iconify/react';
-import { INSTAGRAMICON, GITHUBICON, LINKEDINICON, GMAILICON } from '../../../../constants/images'
-import { BracketOpenIcon, SlashIcon, BracketCloseIcon } from '../../../../constants/icons'
+import React from "react";
+import Styles from "./SocialLinks.module.scss";
+import { Icon } from "@iconify/react";
+import {
+  INSTAGRAMICON,
+  GITHUBICON,
+  LINKEDINICON,
+  GMAILICON,
+} from "../../../../constants/images";
+import {
+  BracketOpenIcon,
+  SlashIcon,
+  BracketCloseIcon,
+} from "../../../../constants/icons";
 
 function SocialLinks() {
-    return (
-        <div className={Styles.o_social}>
-            <Icon className={Styles.o_socialicon} icon={BracketOpenIcon} color="#ababab" />
-            <Icon className={Styles.o_socialicon} icon={SlashIcon} color="#ababab" />
-            <a href="https://www.instagram.com/santigl09/">
-                <img src={INSTAGRAMICON} alt="instagram logo" />
-            </a>
-            <a href="https://github.com/Sagalo04">
-                <img src={GITHUBICON} alt="github logo" />
-            </a>
+  return (
+    <div className={Styles.o_social}>
+      <Icon
+        className={Styles.o_socialicon}
+        icon={BracketOpenIcon}
+        color="#ababab"
+      />
+      <Icon className={Styles.o_socialicon} icon={SlashIcon} color="#ababab" />
+      <a
+        href="https://www.instagram.com/santigl09/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={INSTAGRAMICON} alt="instagram logo" />
+      </a>
+      <a href="https://github.com/Sagalo04" target="_blank" rel="noreferrer">
+        <img src={GITHUBICON} alt="github logo" />
+      </a>
 
-            <a href="https://github.com/Sagalo04">
-                <img src={LINKEDINICON} alt="linkedin logo" />
-            </a>
+      <a href="https://github.com/Sagalo04" target="_blank" rel="noreferrer">
+        <img src={LINKEDINICON} alt="linkedin logo" />
+      </a>
 
-            <a href="mailto:santiago_garcia09@live.com">
-                <img src={GMAILICON} alt="gmail logo" />
-            </a>
+      <a
+        href="mailto:santiago_garcia09@live.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={GMAILICON} alt="gmail logo" />
+      </a>
 
-            <Icon className={Styles.o_socialicon} icon={BracketCloseIcon} color="#ababab" />
-        </div>
-    );
+      <Icon
+        className={Styles.o_socialicon}
+        icon={BracketCloseIcon}
+        color="#ababab"
+      />
+    </div>
+  );
 }
 
 export default SocialLinks;
